91f394f84f85e8e62bc8373f28b956ecb56bf906
Fatima-Anis <rude.fati@gmail.com>
2020-09-30 10:17:24 +0500

    my first folder

---
+++ file.js
@@ -0,0 +1,21 @@
+// var name = "asma"
+// var myage=" iam 23 years old"
+// var myedu ="i have done graduation"
+// console.log(`my name is ${name} and my age is 
+// ${myage} and my qaulification is
+//  ${myedu}`) 
+
+
+
+var age = prompt("enter your age");
+if(age == 20){
+    console.log(" your age is 20 ")
+}
+else if (age < 20){
+    console.log
+        (" you are less than 20")
+    
+}
+else if (age>20){
+    console.log("you are greather than 20")
+}
